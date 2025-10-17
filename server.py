623908f89533912f447e6d27412c23986d4a195b
Semen Zamai <23010815+SemenZamay@users.noreply.github.com>
2025-10-17 12:00:16 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 # server.py
 import os
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 clients = set()
 
 @app.websocket("/ws")
